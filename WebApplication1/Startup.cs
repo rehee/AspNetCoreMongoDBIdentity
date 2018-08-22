4a81862c69bb03b6d93fa0bd383a04be68d8886e
Xudong Hao <Xudong.Hao@deltafs.co.uk>
2018-08-22 15:54:27 +0100

    创建用户流程完成

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -15,80 +15,76 @@ using MongoDB.Driver;
 
 namespace WebApplication1
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IUserStore<MUser>>(provider =>
-            {
-                var client = new MongoClient("mongodb://localhost:27017");
-                var database = client.GetDatabase("lalala");
-
-                return new MUserStore<MUser>(database);
-            });
-            services.AddScoped<IRoleStore<MRole>>(provider =>
-            {
-                var client = new MongoClient("mongodb://localhost:27017");
-                var database = client.GetDatabase("lalala");
-
-                return new MRoleStore<MRole>();
-            });
-            services.AddScoped<IUserRoleStore<MUser>>(provider =>
-            {
-                var client = new MongoClient("mongodb://localhost:27017");
-                var database = client.GetDatabase("lalala");
-
-                return new MUserRoleStore<MUser>();
-            });
-
-            services.AddScoped<UserManager<MUser>>();
-            services.AddScoped<RoleManager<MRole>>();
-            services.AddIdentity<MUser, MRole>(option =>
-            {
-                
-            }).AddDefaultTokenProviders();
-
-            services.AddMvc()
-                .AddRazorPagesOptions(options =>
-                {
-                    options.Conventions.AuthorizeFolder("/Account/Manage");
-                    options.Conventions.AuthorizePage("/Account/Logout");
-                });
-
-            // Register no-op EmailSender used by account confirmation and password reset during development
-            // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=532713
-            services.AddSingleton<IEmailSender, EmailSender>();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
-
-            app.UseStaticFiles();
-
-            app.UseAuthentication();
-
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      //services.AddDbContext<ApplicationDbContext>(options =>
+      //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddSingleton<IMongoDatabase>(s =>
+      {
+        var client = new MongoClient("mongodb://localhost:27017");
+        var database = client.GetDatabase("lalala");
+        return database;
+      });
+      //services.AddScoped<IUserStore<MUser>>(provider =>
+      //      {
+      //        var client = new MongoClient("mongodb://localhost:27017");
+      //        var database = client.GetDatabase("lalala");
+
+      //        return new MUserStore<MUser>(database);
+      //      });
+      services.AddScoped<IUserStore<MUser>, MUserStore<MUser>>();
+      services.AddScoped<IRoleStore<MRole>, MRoleStore<MRole>>();
+      services.AddScoped<IUserRoleStore<MUser>, MUserRoleStore<MUser, MRole, MUserRole>>();
+
+      services.AddScoped<UserManager<MUser>>();
+      services.AddScoped<RoleManager<MRole>>();
+      services.AddIdentity<MUser, MRole>(option =>
+      {
+
+      }).AddDefaultTokenProviders();
+
+      services.AddMvc()
+          .AddRazorPagesOptions(options =>
+          {
+            options.Conventions.AuthorizeFolder("/Account/Manage");
+            options.Conventions.AuthorizePage("/Account/Logout");
+          });
+
+      // Register no-op EmailSender used by account confirmation and password reset during development
+      // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=532713
+      services.AddSingleton<IEmailSender, EmailSender>();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseBrowserLink();
+        app.UseDeveloperExceptionPage();
+        app.UseDatabaseErrorPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseAuthentication();
+
+      app.UseMvc();
+    }
+  }
 }
