337970ab4a470f60d1c0f88d1fa6e14d5efd5044
Xudong Hao <Xudong.Hao@deltafs.co.uk>
2018-10-16 08:53:26 +0100

    文件上传下载试验代码

--- SDHCCContent/Controllers/TestController.cs
+++ SDHCCContent/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -32,6 +33,48 @@ namespace SDHCCContent.Controllers
     public IActionResult Multi(IEnumerable<string> select)
     {
       return View();
+    }
+    public async Task<IActionResult> File(string id)
+    {
+      var filename = id;
+      if (filename == null)
+        return Content("filename not present");
+
+      var path = Path.Combine(
+                     Directory.GetCurrentDirectory(),
+                     "wwwroot", filename);
+
+      var memory = new MemoryStream();
+      using (var stream = new FileStream(path, FileMode.Open))
+      {
+        await stream.CopyToAsync(memory);
+      }
+      memory.Position = 0;
+      return File(memory, GetContentType(path), Path.GetFileName(path));
+    }
+    private string GetContentType(string path)
+    {
+      var types = GetMimeTypes();
+      var ext = Path.GetExtension(path).ToLowerInvariant();
+      return types[ext];
+    }
+
+    private Dictionary<string, string> GetMimeTypes()
+    {
+      return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"}
+            };
     }
   }
   public class TestPage : ContentBaseModel
